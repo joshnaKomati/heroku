27d7c78419770115b6300b69c852fabbca59b247
joshna komati <komatijoshna123@gmail.com>
2022-11-02 15:57:28 +0530

    4

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express=require("express")
 const app=express()
 const PORT= process.env.PORT || 4000
+app.set('port',PORT)
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 require("./configure/db")
 const mainRouter=require("./Router/index")
-app.set('ejs','view engine')
+// app.set('ejs','view engine')
 app.use("/api",mainRouter)
 app.get("/",(req,res)=>{
     res.send("hello world")
